857365518b6cac935f2539f8bead6054c71f4a12
Angela Garcia <danigarcia_1@hotmail.com>
2021-03-13 16:39:53 -0500

    avances

--- lib/repository/repository.dart
+++ lib/repository/repository.dart
@@ -1,23 +1,25 @@
+import 'dart:convert';
+
 import 'package:http/http.dart';
 import 'package:linkedin/models/post_model.dart';
 
-class Repository{
+class Repository {
   final String postsURL = "https://jsonplaceholder.typicode.com/posts";
+  final String postGet = "http://34.121.81.88:8070/posts/getAll";
 
-  Future<List<Post>> getPosts() async {
-    Response res = await get(postsURL);
-
+  Future<List<PostModel>> getPosts() async {
+    print("xx2");
+    Response res = await get(postGet);
     if (res.statusCode == 200) {
       List<dynamic> body = jsonDecode(res.body);
-
-      List<Post> posts = body
+      List<PostModel> posts = body
           .map(
-            (dynamic item) => Post.fromJson(item),
-      )
+            (dynamic item) => PostModel.fromJson(item),
+          )
           .toList();
-
       return posts;
     } else {
+      print("xx1");
       throw "Can't get posts.";
     }
   }
